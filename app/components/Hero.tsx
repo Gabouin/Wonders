9fc035109050666e130fce3a133b158d8191d634
Ridit <riditjangra09@gmail.com>
2026-08-25 22:03:03 +0530

    chore: hide dash button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -19,13 +19,13 @@ export function Hero() {
           className="absolute z-20 bottom-6 sm:bottom-10 left-1/2 -translate-x-1/2 w-40 sm:w-52 md:w-66 h-auto group-hover:w-44 sm:group-hover:w-58 md:group-hover:w-77 transition-all"
         />
       </a>
-      <a href="/dashboard" className="group">
+      {/* <a href="/dashboard" className="group">
         <img
           src="/open-dash.png"
           alt="open dash"
           className="absolute z-20 top-6 sm:bottom-10 right-4 w-30 sm:w-42 md:w-56 h-auto group-hover:w-34 sm:group-hover:w-48 md:group-hover:w-67 transition-all"
         />
-      </a>
+      </a> */}
     </div>
   );
 }
