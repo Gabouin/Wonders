6fedc03d24f64d12a867ff9313bf0900162132bd
Ridit <riditjangra09@gmail.com>
2026-08-25 18:09:02 +0530

    feat: add rsvp button

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 export function Hero() {
   return (
     <div
       className="w-full h-screen bg-cover bg-center"
       style={{ backgroundImage: "url('/hero-bg.png')" }}
-    ></div>
+    >
+      <a
+        href="https://rsvp.hackclub.community/wonders"
+        target="_blank"
+        className="group"
+      >
+        <img
+          src="/rsvp.png"
+          alt="rsvp"
+          className="absolute z-20 bottom-10 left-[50%] translate-x-[-50%] w-66 h-auto group-hover:w-77 transition-all"
+        />
+      </a>
+    </div>
   );
 }
